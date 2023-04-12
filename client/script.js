bb0be3e1575fcc780f009c42957e5e4ddd0ea707
parkminwoo <parkminwoo1991@gmail.com>
2023-04-13 01:29:45 +0900

    refactor: merge client and server side

--- client/script.js
+++ client/script.js
@@ -3,7 +3,7 @@ import user from "./assets/user.svg";
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
-const serverApi = "http://localhost:5000/";
+const serverApi = "http://localhost:8000/";
 
 let loadInterval;
 
